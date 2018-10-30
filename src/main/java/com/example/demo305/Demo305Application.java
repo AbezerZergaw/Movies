9fd2e3670275ef0f3f9f31ad36b341711dabfcef
GBTC440013UR <GBTC440013UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-30 17:00:07 -0400

    Initial commit

---
+++ src/main/java/com/example/demo305/Demo305Application.java
@@ -0,0 +1,12 @@
+package com.example.demo305;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo305Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo305Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/demo305/Demo305ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demo305;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo305ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
